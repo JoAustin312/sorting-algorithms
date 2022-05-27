ee7cdab1abc560fcc77b72a88581159a2a1f19bd
JoAustin312 <65620601+JoAustin312@users.noreply.github.com>
2022-05-27 18:19:41 +0100

    small additions to bubblesort

--- src/main/java/com/sparta/ja/bubblesort/BubbleSort.java
+++ src/main/java/com/sparta/ja/bubblesort/BubbleSort.java
@@ -10,12 +10,14 @@ public class BubbleSort
     public static void bubbleSort(int[] arr){
         int n = arr.length;
         int tempNum = 0;
-        for (int i = 0; i < n; i++){
-            for (int j = 1; j < (n-i); j++){
-                if (arr[j-1] > arr[j]){
-                    tempNum = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = tempNum;
+        if (arr.length > 0) { //not sure if this check is necessary
+            for (int i = 0; i < n; i++){
+                for (int j = 1; j < (n-i); j++){
+                    if (arr[j-1] > arr[j]){
+                        tempNum = arr[j-1];
+                        arr[j-1] = arr[j];
+                        arr[j] = tempNum;
+                    }
                 }
             }
         }
--- src/test/java/com/sparta/ja/bubblesort/BubbleSortTest.java
+++ src/test/java/com/sparta/ja/bubblesort/BubbleSortTest.java
@@ -7,11 +7,58 @@ import org.junit.jupiter.api.Test;
 public class BubbleSortTest
 {
     @Test
-    @DisplayName("check int array is being sorted")
-    void checkIntArrayIsBeingSorted(){
+    @DisplayName("check int array 5 elements is being sorted")
+    void checkIntArray5ElementsIsBeingSorted(){
+        int[] expected = {2,48,94,152,285};
+        int[] actual = {2,94,285,48,152};
+        BubbleSort.bubbleSort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("check int array negative elements is being sorted")
+    void checkIntNegativeElementsIsBeingSorted(){
+        int[] expected = {-285,-152,-94,-52,-48,-34,-10,-6,-2};
+        int[] actual = {-2,-52,-94,-6,-34,-285,-10,-48,-152};
+        BubbleSort.bubbleSort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("check int array negative and positive elements is being sorted")
+    void checkIntNegativeAndPositiveElementsIsBeingSorted(){
+        int[] expected = {-285,-94,-48,-34,-2,5,19,64,192};
+        int[] actual = {-2,5,-94,64,-34,-285,19,-48,192};
+        BubbleSort.bubbleSort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("check int array 9 elements is being sorted")
+    void checkIntArray9ElementsIsBeingSorted(){
         int[] expected = {2,6,10,34,48,52,94,152,285};
         int[] actual = {2,52,94,6,34,285,10,48,152};
         BubbleSort.bubbleSort(actual);
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("check empty array does not throw error") //not sure if this test is necessary
+    void checkEmptyArrayDoesNotThrowError(){
+        int[] expected = {};
+        int[] actual = {};
+        BubbleSort.bubbleSort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("check int array 1 element is returning same element")
+    void checkIntArray1ElementIsReturningSameElement(){
+        int[] expected = {5};
+        int[] actual = {5};
+        BubbleSort.bubbleSort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
 }
